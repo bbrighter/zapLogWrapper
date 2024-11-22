9006574f17130e470caacf11bdd06f7c4aa9be19
Benjamin Heller <benjamin.heller@mein.gmx>
2024-11-22 17:26:30 +0100

    feat: better default options

--- loggerOptions.go
+++ loggerOptions.go
@@ -1,6 +1,12 @@
 package logger
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime/debug"
+	"strings"
+)
 
 type LoggerOptions struct {
 	level     LogLevel
@@ -10,14 +16,17 @@ type LoggerOptions struct {
 	service   string
 }
 
+// Creates default options with level info, folder current directory/logs, file name = "logs.log", namespace = "namespace"
 func NewLoggerOptions() *LoggerOptions {
-	homeDir, _ := os.UserHomeDir()
-	return &LoggerOptions{
-		level:     Info,
-		folder:    homeDir,
-		fileName:  "logs.log",
-		namespace: "namespace",
+	opts := &LoggerOptions{
+		level:    Info,
+		fileName: "logs",
 	}
+	wd, _ := os.Getwd()
+	opts.SetFolder(wd)
+	service, serviceType, _ := getModuleName()
+	opts.SetServiceAndAppName(service, ServiceType(serviceType))
+	return opts
 }
 
 func (o *LoggerOptions) SetLevel(level LogLevel) {
@@ -25,11 +34,12 @@ func (o *LoggerOptions) SetLevel(level LogLevel) {
 }
 
 func (o *LoggerOptions) SetFolder(folder string) {
-	o.folder = folder
+	o.folder = path.Join(folder, "logs")
 }
 
+// Sets the log file to filename.log
 func (o *LoggerOptions) SetFileName(fileName string) {
-	o.fileName = fileName
+	o.fileName = fileName + ".log"
 }
 
 type ServiceType string
@@ -42,4 +52,21 @@ const (
 func (o *LoggerOptions) SetServiceAndAppName(namespace string, serviceType ServiceType) {
 	o.namespace = namespace
 	o.service = namespace + "-" + string(serviceType)
+}
+
+func getModuleName() (string, string, error) {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "", "", fmt.Errorf("failed to read build info")
+	}
+	moduleName := path.Base(info.Main.Path)
+	splits := strings.Split(moduleName, "-")
+	service := splits[0]
+	if len(splits) > 1 {
+		serviceType := splits[1]
+		if serviceType == "api" || serviceType == "ui" {
+			return service, serviceType, nil
+		}
+	}
+	return service, "", nil
 }
